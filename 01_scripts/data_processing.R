6493796daaa740b14f2c64a00e9beb406903f1d5
Altare <David.Altare@Waterboards.ca.gov>
2022-08-26 13:15:57 -0700

    update process for compiling 5102 data across all years, and add 2021 5102 data

--- 01_scripts/data_processing.R
+++ 01_scripts/data_processing.R
@@ -1,82 +1,123 @@
-# load packages -----------------------------------
+
+# load packages -----------------------------------------------------------
+library(tidyverse)
 library(readxl)
-library(readr)
-library(dplyr)
 library(janitor)
 library(here)
-library(purrr)
 library(glue)
 library(lubridate)
+library(archive)
+library(ckanr)
 
 
-# !!!!!!!!!!!!!!! ENTER THE RANGE OF YEARS WITH 5102 REPORTS (start_year:end_year) !!!!!!!!!!!!!!!
-year_range <- 2011:2020
 
+# read data ---------------------------------------------------------------
+## get list of files in the '02_data_raw/5102' folder
+files_list <- list.files(here('02_data_raw', '5102'))
 
-# read data into R -----------------------------------
-df_5102_report <- map_df(.x = year_range, 
-                         .f = ~ read_excel(here('02_data_raw', 
-                                                glue('calhr-5102-statewide-', .x, '.xlsx')),
-                                           col_types = 'text')) %>% 
-    type_convert() 
+## verify the files to be used
+## (should be excel files with name 'calhr-5102-statewide-YYYY.xlsx')
+files_valid <- str_detect(string = files_list, pattern = 'calhr-5102-statewide-\\d{4}.xlsx')
+files_list <- files_list[files_valid]
 
-# save the original column names - may want to revert back to these when saving the output file
-names_df_5102_report <- names(df_5102_report) 
+## read data
+df_5102_report <- map_df(.x = list.files(here('02_data_raw', '5102')), 
+                         .f = ~ read_excel(here('02_data_raw', '5102', .x),
+                                           col_types = 'text') %>% 
+                             # standardize field names (remove spaces)
+                             `names<-`(str_remove_all(string = names(.), pattern = ' '))
+) %>% 
+    type_convert()
 
 # clean up the column names to make them easier to work with in R
 df_5102_report <- df_5102_report %>% 
-    clean_names() 
-
-# to check an individual year's file
-    # year <- 2019
-    # df_year <- read_excel(path = here('02_data_raw', glue('calhr-5102-statewide-', year, '.xlsx')),
-    #                       col_types = 'text') #%>% 
-    #     #type_convert()
-    # head(df_year) # view the first couple of records
-    # tail(df_year) # view the last couple of records
+    clean_names()
 
 
 
-# re-format data -----------------------------------
-# fix dates
-# check the number of NAs in the original dataset (to be sure there's a value for each record)
+# re-format data ----------------------------------------------------------
+## fix dates ----
+### check original dataset (to be sure there's a value for each record)
 sum(is.na(df_5102_report$as_of_date))
-# convert the dates (it's okay if there are warning messages from this step, as long as the checks below look okay)
+df_5102_report %>% count(as_of_date)
+
+### convert the dates 
+### (it's okay if there are warning messages from this step, as long as the checks below look okay)
 df_5102_report <- df_5102_report %>% 
     mutate(as_of_date = case_when(!is.na(mdy(as_of_date)) ~ 
                                       mdy(as_of_date),
                                   !is.na(excel_numeric_to_date(as.numeric(as_of_date))) ~ 
                                       excel_numeric_to_date(as.numeric(as_of_date)),
                                   TRUE ~ NA_Date_))
-# check to make sure the conversion worked
+
+### check to make sure the conversion worked
 sum(is.na(df_5102_report$as_of_date)) # should be the same as the number above, probably zero
-range(df_5102_report$as_of_date) # check to make sure the new dates are within the correct range
+df_5102_report %>% count(as_of_date)
 
 
 
 # write the processed data to a new file -----------------------------------
-# revert back to the original names (assuming that we want the output dataset to have the same column names as the source datasets)
-names(df_5102_report) <- names_df_5102_report
+## revert back to the original names 
+## (assuming that we want the output dataset to have the same column names as the source datasets)
+df_5102_report <- df_5102_report %>% 
+    rename('As Of Date' = as_of_date, 'Dept' = dept,
+           'Employee Category' = employee_category, 'Sub Category' = sub_category, 
+           'SOC Code' = soc_code, 'Scheme Code' = scheme_code, 
+           'Class Code' = class_code, 'Class Title' = class_title, 
+           'Identity Variable' = identity_variable, 'Gender' = gender,
+           'Record Count' = record_count)
 
-# write the data to the '03_data_processed' folder
-# NOTE: writing the data to a gzip file rather than a regular csv to save space - you can 
-# read/write using this format directly with R using the readr package, and you can extract 
-# it to a regular csv using 7zip (or some other software)
-write_csv(x = df_5102_report, 
-          file = here('03_data_processed', 
-                      glue('calhr_5102_statewide_', 
-                           year_range[1], 
-                           '-', 
-                           year_range[length(year_range)], 
-                           '.csv.gz')))
+## write the data to the '03_data_processed' folder
+## NOTE: writing the data to a zip file rather than a regular csv to save space - you can 
+## read/write using this format directly with R using the readr package, or extract 
+# it to a regular csv
+out_file <- paste0('calhr_5102_statewide_', 
+                   year(min(df_5102_report$`As Of Date`)), 
+                   '-', 
+                   year(max(df_5102_report$`As Of Date`)))
 
-# also writing  a copy of the data directly to the shiny folder, since all of the code/data for 
-# the app needs to be contained within a single folder in order to load to shinyapps.io
+### zip file (zipped csv)
 write_csv(x = df_5102_report, 
-          file = here('05_shiny_app', 
-                      'data', 
-                      glue('calhr_5102_statewide_', 
-                           year_range[1], 
-                           '-', 
-                           year_range[length(year_range)], 
-                           '.csv.gz')))
+          file = archive_write(here('03_data_processed', 
+                                    paste0(out_file,'.zip')), 
+                               paste0(out_file, '.csv')))
+
+### gz file
+# write_csv(x = df_5102_report, 
+#           file = here('03_data_processed', 
+#                       paste0(out_file, '.csv.gz')))
+
+
+## also write a copy data directly to the shiny folder, since all of the code/data for 
+## the app needs to be contained within a single folder in order to load to shinyapps.io
+write_csv(x = df_5102_report,
+          file = here('05_shiny_app',
+                      paste0(out_file, '.csv.gz')))
+
+
+
+# upload to open data portal ----------------------------------------------
+## Note: As an alternative to the steps below, you can do this by extracting
+## the csv from the zip file created above, then manually load that csv file
+## to the data.ca.gov portal, at: 
+## https://data.ca.gov/dataset/calhr-civil-rights-data-for-gare-capital-cohort-2019/resource/aba87ad9-f6b0-4a7e-a45e-d1452417eb7f
+
+## write data to temp csv file ----
+temp_dir <- tempdir()
+write_csv(x = df_5102_report, 
+          file = file.path(temp_dir,  paste0(out_file, '.csv')))
+
+## set the ckan defaults ----
+### get data portal API key 
+### (assuming this is saved in the local environment, in a variable called: data_portal_key)
+### (it's available on data.ca.gov by going to your user profile)
+portal_key <- Sys.getenv('data_portal_key')
+resource_id <- 'aba87ad9-f6b0-4a7e-a45e-d1452417eb7f' # https://data.ca.gov/dataset/calhr-civil-rights-data-for-gare-capital-cohort-2019/resource/aba87ad9-f6b0-4a7e-a45e-d1452417eb7f
+
+ckanr_setup(url = 'https://data.ca.gov/', 
+            key = portal_key) 
+
+## upload file to portal ----
+file_upload <- resource_update(id = resource_id, 
+                               path = file.path(temp_dir,  
+                                                paste0(out_file, '.csv')))
--- README.md
+++ README.md
@@ -14,6 +14,14 @@ Description of the dataset coming soon...
 
 If using RStudio, open the `workforce_data.Rproj` file to open the project within RStudio. Otherwise, set your working directory in R to the folder that contains this readme file.
 
+### Updating 5102 Dataset
+
+To update the compiled 5102 dataset with data for an additional year:
+
+1.  Get the raw data for the new year from CalHR.
+2.  Save the new data to an excel file named "calhr-5102-statewide-YYYY.xlsx" (where YYYY is the year the dataset covers), and put that file in the `02_data_raw/5102` folder.
+3.  Run the `01_scripts/data_processing.R` script. This script compiles the data from each of the individual years' data files (assuming they are saved with the file naming convention described in step 2), and saves the compiled dataset to a zipped csv file in the `03_data_processed` folder. It also updates the [compiled dataset on the CA Open Data Portal](https://data.ca.gov/dataset/calhr-civil-rights-data-for-gare-capital-cohort-2019/resource/aba87ad9-f6b0-4a7e-a45e-d1452417eb7f) (the script assumes that you have a data portal key saved to your local environment, in a variable named `data_portal_key`; as an alternative to using this script to update the data portal, you can manually update the portal by extracting the zipped csv in the `03_data_processed` folder and loading it to the portal).
+
 ### Exploratory Data Analysis
 
 To run the exploratory data analysis script, run the `workforce_data_exploration.R` script in the `01_scripts` folder.
@@ -24,9 +32,11 @@ To create graphs visualizing your department's workforce demographics, use the s
 
 ## Reports / Slides
 
-- [2020 Workforce Data Analysis slides](https://caleparacialequity.github.io/ca-state-workforce-data/07_slides/2021-05-14/workforce_data_summary_2021-05-14.html)
-   
-- [5102 Data Analysis by Agency / Department](https://caleparacialequity.github.io/ca-state-workforce-data/06_reports/workforce_metrics_5102.html)
-- PowerBI prototypes and older data viz tools:
-   - CalHR statewide data tools (includes up to 2020 for 5102 data. https://bit.ly/ca_workforce_2020
-   - CalEPA interactive tool for 2011 to 2020 CalHR 5102 data: https://bit.ly/CalEPA_workforcedemo_PeterH
+-   [2020 Workforce Data Analysis slides](https://caleparacialequity.github.io/ca-state-workforce-data/07_slides/2021-05-14/workforce_data_summary_2021-05-14.html)
+
+-   [5102 Data Analysis by Agency / Department](https://caleparacialequity.github.io/ca-state-workforce-data/06_reports/workforce_metrics_5102.html)
+
+-   PowerBI prototypes and older data viz tools:
+
+    -   CalHR statewide data tools (includes up to 2020 for 5102 data. <https://bit.ly/ca_workforce_2020>
+    -   CalEPA interactive tool for 2011 to 2020 CalHR 5102 data: <https://bit.ly/CalEPA_workforcedemo_PeterH>
